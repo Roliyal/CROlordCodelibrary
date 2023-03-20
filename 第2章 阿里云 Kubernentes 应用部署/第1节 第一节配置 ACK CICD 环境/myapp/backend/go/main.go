f1722a3207ab1a2fe81c732864d4f2f3d060a585
issac <issac@roliyal.com>
2023-03-20 20:08:27 +0800

    code1

--- 第2章 阿里云 Kubernentes 应用部署/第1节 第一节配置 ACK CICD 环境/myapp/backend/go/main.go
+++ 第2章 阿里云 Kubernentes 应用部署/第1节 第一节配置 ACK CICD 环境/myapp/backend/go/main.go
@@ -1,81 +1,96 @@
 package main
 
 import (
-    "github.com/gin-gonic/gin"
-    "github.com/dgrijalva/jwt-go"
-    "net/http"
-    "time"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
 )
 
 type User struct {
-    ID       uint64 `json:"id"`
-    Email    string `json:"email"`
-    Password string `json:"password"`
+	Username string `json:"username"`
+	Password string `json:"password"`
 }
+
 type GameResult struct {
-    ID        uint64    `json:"id"`
-    UserID    uint64    `json:"user_id"`
-    Score     uint64    `json:"score"`
-    CreatedAt time.Time `json:"created_at"`
+	Username   string `json:"username"`
+	Score      int    `json:"score"`
+	CreateTime int64  `json:"create_time"`
 }
+
+var secretKey = []byte("secret_key")
 
 func main() {
-    // 初始化 Gin 框架
-    r := gin.Default()
+	r := gin.Default()
 
-    // 用户注册
-    r.POST("/register", func(c *gin.Context) {
-        var user User
-        if err := c.ShouldBindJSON(&user); err != nil {
-            c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-            return
-        }
+	// 登录认证接口
+	r.POST("/login", func(c *gin.Context) {
+		var user User
+		if err := c.BindJSON(&user); err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
 
-        // 在此处添加用户注册逻辑，例如将用户信息保存到数据库中
-        // ...
+		// 检查用户名和密码是否正确，这里省略具体实现
 
-        c.Status(http.StatusOK)
-    })
+		// 生成 JWT
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+			"username": user.Username,
+			"exp":      time.Now().Add(time.Hour * 24).Unix(), // 过期时间为 24 小时
+		})
 
-    // 用户登录
-    r.POST("/login", func(c *gin.Context) {
-        var user User
-        if err := c.ShouldBindJSON(&user); err != nil {
-            c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-            return
-        }
+		tokenString, err := token.SignedString(secretKey)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "failed to generate token"})
+			return
+		}
 
-        // 在此处添加用户登录逻辑，例如从数据库中查找用户信息并验证密码是否正确
-        // ...
+		c.JSON(http.StatusOK, gin.H{"token": tokenString})
+	})
 
-        // 生成 JWT
-        token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-            "id": user.ID,
-            "exp": time.Now().Add(time.Hour * 24).Unix(),
-        })
-        tokenString, err := token.SignedString([]byte("your-secret-key"))
-        if err != nil {
-            c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to generate JWT"})
-            return
-        }
+	// 需要登录认证的接口
+	auth := r.Group("/", JWTAuthMiddleware())
+	{
+		// 保存游戏成绩接口
+		auth.POST("/game/result", func(c *gin.Context) {
+			var result GameResult
+			if err := c.BindJSON(&result); err != nil {
+				c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+				return
+			}
 
-        c.JSON(http.StatusOK, gin.H{"token": tokenString})
-    })
+			// 保存游戏成绩到数据库，这里省略具体实现
 
-    // 游戏成绩保存
-    r.POST("/game_result", func(c *gin.Context) {
-        var gameResult GameResult
-        if err := c.ShouldBindJSON(&gameResult); err != nil {
-            c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-            return
-        }
+			c.Status(http.StatusOK)
+		})
+	}
 
-        // 在此处添加保存游戏成绩逻辑，例如将游戏成绩信息保存到数据库中
-        // ...
+	// 启动服务器
+	if err := r.Run(":8080"); err != nil {
+		panic(err)
+	}
+}
 
-        c.Status(http.StatusOK)
-    })
+// JWT 认证中间件
+func JWTAuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "missing token"})
+			return
+		}
 
-    // 启动服务器
-    r.Run(":8080")
+		tokenString := authHeader[len("Bearer "):]
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			return secretKey, nil
+		})
+		if err != nil || !token.Valid {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "invalid token"})
+			return
+		}
+
+		c.Next()
+	}
 }
