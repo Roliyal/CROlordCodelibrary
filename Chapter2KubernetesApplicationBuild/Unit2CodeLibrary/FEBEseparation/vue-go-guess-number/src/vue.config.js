aa969b5d2cec193d66408f4fbcf799c807d84da6
issac <issac@roliyal.com>
2024-04-22 17:44:13 +0800

    Update default.conf

--- Chapter2KubernetesApplicationBuild/Unit2CodeLibrary/FEBEseparation/vue-go-guess-number/src/vue.config.js
+++ Chapter2KubernetesApplicationBuild/Unit2CodeLibrary/FEBEseparation/vue-go-guess-number/src/vue.config.js
@@ -6,7 +6,7 @@ module.exports = {
   devServer: {
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:8081",
+        target: "http://app-go-backend-service.cicd.svc.cluster.local:8081/check-guess",
         changeOrigin: true,
         pathRewrite: {
           "^/api": "",
