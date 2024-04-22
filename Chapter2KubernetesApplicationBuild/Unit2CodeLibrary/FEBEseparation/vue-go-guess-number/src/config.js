f010dd4e36a5b9e5c5be949a8882155509790472
issac <issac@roliyal.com>
2024-04-22 13:50:59 +0800

    Update default.conf

--- Chapter2KubernetesApplicationBuild/Unit2CodeLibrary/FEBEseparation/vue-go-guess-number/src/config.js
+++ Chapter2KubernetesApplicationBuild/Unit2CodeLibrary/FEBEseparation/vue-go-guess-number/src/config.js
@@ -2,7 +2,7 @@
 const isProduction = process.env.NODE_ENV === "production";
 
 const config = {
-    apiUrl: isProduction ? "/check-guess" : "http://127.0.0.1:8081/check-guess",
+    apiUrl: isProduction ? "http://app-go-backend-service.cicd.svc.cluster.local:8081/check-guess" : "http://127.0.0.1:8081/check-guess",
 };
 
 export default config;
