2a63ebf13efe50604ac127d2ebf8208cfac175a0
issac <issac@roliyal.com>
2024-06-11 18:58:26 +0800

    Update

--- Chapter2KubernetesApplicationBuild/Unit2CodeLibrary/Microservice/front-guess/src/config.js
+++ Chapter2KubernetesApplicationBuild/Unit2CodeLibrary/Microservice/front-guess/src/config.js
@@ -1,4 +1,4 @@
-const baseURL = "http://8.217.105.200:80"; // 使用您的 Traefik 外部 IP 和端口
+const baseURL = "http://47.239.11.248:80"; // 使用您的 Traefik 外部 IP 和端口
 
 export default {
     loginURL: `${baseURL}`,
