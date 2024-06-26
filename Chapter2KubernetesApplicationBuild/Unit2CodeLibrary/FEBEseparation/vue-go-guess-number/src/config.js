487c7910f87da54812e9028df2d576639375f48f
issac <issac@roliyal.com>
2024-06-26 18:38:27 +0800

    Update

--- Chapter2KubernetesApplicationBuild/Unit2CodeLibrary/FEBEseparation/vue-go-guess-number/src/config.js
+++ Chapter2KubernetesApplicationBuild/Unit2CodeLibrary/FEBEseparation/vue-go-guess-number/src/config.js
@@ -1,7 +1,8 @@
 // config.js
 const config = {
-    apiUrl: "/check-guess" // 使用Nginx location块中指定的相同路径
-    //apiUrl: "http://47.242.207.65/check-guess"    // 使用oss块中指定的后端网关地址
+    //apiUrl: "/check-guess" // 使用Nginx location块中指定的相同路径
+    apiUrl: "http://8.217.25.74/check-guess"    // 使用oss块中指定的后端网关地址
+
 };
 
 export default config;
