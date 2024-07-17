3ddb2421889a0292efa57e1cf09d99d4fa28b27f
issac <issac@roliyal.com>
2024-07-17 18:55:29 +0800

    Update

--- Chapter4MicroserviceGovern/Unit2CodeLibrary/FEBEseparation/vue-go-guess-number/src/main.js
+++ Chapter4MicroserviceGovern/Unit2CodeLibrary/FEBEseparation/vue-go-guess-number/src/main.js
@@ -1,5 +1,19 @@
+/*
 import { createApp } from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+createApp(App).mount('#app')
+*/
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+const environment = process.env.VUE_APP_ENV;
+if (environment === 'gray') {
+    import('./gray/css/main.css');
+} else {
+    import('./base/css/main.css');
+}
 
 createApp(App).mount('#app')
--- Chapter4MicroserviceGovern/Unit2CodeLibrary/FEBEseparation/vue-go-guess-number/src/vue.config.js
+++ Chapter4MicroserviceGovern/Unit2CodeLibrary/FEBEseparation/vue-go-guess-number/src/vue.config.js
@@ -1,3 +1,4 @@
+/*
 // vue.config.js
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
@@ -12,4 +13,29 @@ module.exports = {
     },
   },
   productionSourceMap: false,
+};
+*/
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://app-go-backend-service.cicd.svc.cluster.local:8081',  // 指向后端服务的内部 IP 或域名
+        changeOrigin: true,
+        pathRewrite: { '^/api': '' },  // 重写路径，去掉 /api
+      },
+    },
+  },
+  productionSourceMap: false,
+  configureWebpack: config => {
+    config.plugins.forEach(plugin => {
+      if (plugin instanceof HtmlWebpackPlugin) {
+        plugin.userOptions.templateParameters = {
+          ...plugin.userOptions.templateParameters,
+          VUE_APP_ENV: process.env.VUE_APP_ENV
+        };
+      }
+    });
+  }
 };
