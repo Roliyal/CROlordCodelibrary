09cfa71b786dde9783f3f38504dd47937c0d8366
issac <issac@roliyal.com>
2025-02-18 17:24:53 +0800

    Update

--- Chapter4MicroserviceGovern/Unit2CodeLibrary/Microservice/front-guess/src/axiosInstance.js
+++ Chapter4MicroserviceGovern/Unit2CodeLibrary/Microservice/front-guess/src/axiosInstance.js
@@ -5,7 +5,7 @@ import store from "./store";
 
 // 创建 Axios 实例
 const axiosInstance = axios.create({
-    baseURL: 'http://47.239.122.54', // 后端服务的基础服务 URL
+    baseURL: 'http://47.86.47.121', // 后端服务的基础服务 URL
     timeout: 10000, // 请求超时时间（毫秒）
     withCredentials: true, // 允许携带凭证（如 cookies）
 });
--- Chapter4MicroserviceGovern/Unit2CodeLibrary/Microservice/front-guess/src/config.js
+++ Chapter4MicroserviceGovern/Unit2CodeLibrary/Microservice/front-guess/src/config.js
@@ -1,4 +1,4 @@
-const baseURL = "http://47.239.122.54:80"; // 使用您的 Traefik 外部 IP 和端口
+const baseURL = "http://47.86.47.121:80"; // 使用您的 Traefik 外部 IP 和端口
 
 export default {
     loginURL: `${baseURL}`,
