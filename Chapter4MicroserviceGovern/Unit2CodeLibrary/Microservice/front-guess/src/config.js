09392bdd2d29751acb0fc5abc6738dc299fd8e89
issac <issac@roliyal.com>
2025-02-24 15:58:13 +0800

    Update

--- Chapter4MicroserviceGovern/Unit2CodeLibrary/Microservice/front-guess/src/config.js
+++ Chapter4MicroserviceGovern/Unit2CodeLibrary/Microservice/front-guess/src/config.js
@@ -1,4 +1,4 @@
-const baseURL = "http://47.86.47.121:80"; // 使用您的 Traefik 外部 IP 和端口
+const baseURL = "http://8.149.243.155:80"; // 使用您的 Traefik 外部 IP 和端口
 
 export default {
     loginURL: `${baseURL}`,
