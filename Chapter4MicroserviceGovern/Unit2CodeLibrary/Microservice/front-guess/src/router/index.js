2baa2e84fd49e815bc1a71cb323056cbac5b8d7b
issac <issac@roliyal.com>
2025-03-07 15:27:43 +0800

    Update

--- Chapter4MicroserviceGovern/Unit2CodeLibrary/Microservice/front-guess/src/router/index.js
+++ Chapter4MicroserviceGovern/Unit2CodeLibrary/Microservice/front-guess/src/router/index.js
@@ -5,13 +5,11 @@ import ScoreboardComponent from "../components/ScoreboardComponent.vue";
 import authApi from "../auth-api"; // 更新这里
 import RegisterComponent from "../components/RegisterComponent.vue"; // 导入 Register 组件
 
-
 const routes = [
     { path: "/login", component: LoginComponent },
     { path: "/register", component: RegisterComponent }, // 添加新的路由
     { path: "/game", component: GuessNumberComponent },
     { path: "/scoreboard", component: ScoreboardComponent },
-
 ];
 
 const router = createRouter({
@@ -20,10 +18,13 @@ const router = createRouter({
 });
 
 router.beforeEach((to, from, next) => {
-    if (to.path !== "/login" && to.path !== "/register" && !authApi.isAuthenticated) {
-        next("/login");
+    if (
+        (to.path !== "/login" && to.path !== "/register") && // 排除登录和注册
+        !authApi.isAuthenticated // 未认证
+    ) {
+        next("/login"); // 重定向到登录页
     } else {
-        next();
+        next(); // 继续其他页面
     }
 });
 
