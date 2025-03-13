c0e23c828140d511dbc1150bd8b177fb6287cd2d
issac <issac@roliyal.com>
2025-03-13 14:31:43 +0800

    Update

--- Chapter4MicroserviceGovern/Unit2CodeLibrary/Microservice/front-guess/src/App.vue
+++ Chapter4MicroserviceGovern/Unit2CodeLibrary/Microservice/front-guess/src/App.vue
@@ -2,13 +2,13 @@
   <div id="app">
     <nav class="navbar">
       <div v-if="!store.state.isLoggedIn">
-        <router-link to="/login">登录</router-link>
+        <router-link to="/login">登录-this is gra</router-link>
         <span>/</span>
         <router-link to="/register">注册-this is gray</router-link>
       </div>
       <a v-else href="#" @click="logout">退出</a>
-      <router-link to="/game">猜数字游戏</router-link>
-      <router-link to="/scoreboard">猜测次数最少排行榜</router-link>
+      <router-link to="/game">猜数字游戏-this is gra</router-link>
+      <router-link to="/scoreboard">猜测次数最少排行榜-this is gra</router-link>
     </nav>
     <router-view></router-view>
   </div>
