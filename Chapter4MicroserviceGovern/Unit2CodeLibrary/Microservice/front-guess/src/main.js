d4b77ea301bdefb78fcc55b33b9740b1121437a0
issac <issac@roliyal.com>
2025-03-14 10:25:13 +0800

    Update

--- Chapter4MicroserviceGovern/Unit2CodeLibrary/Microservice/front-guess/src/main.js
+++ Chapter4MicroserviceGovern/Unit2CodeLibrary/Microservice/front-guess/src/main.js
@@ -2,7 +2,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";  // 确保正确导入 store
+import store from "./store";  // 确保正确导入 store.js
 
 const app = createApp(App);
 
