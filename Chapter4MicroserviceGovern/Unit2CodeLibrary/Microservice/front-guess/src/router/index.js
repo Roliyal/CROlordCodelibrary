c50053caa36600fbaea59e5ed75f3508abcdf6be
issac <issac@roliyal.com>
2025-03-14 10:31:27 +0800

    Update

--- Chapter4MicroserviceGovern/Unit2CodeLibrary/Microservice/front-guess/src/router/index.js
+++ Chapter4MicroserviceGovern/Unit2CodeLibrary/Microservice/front-guess/src/router/index.js
@@ -21,11 +21,11 @@ const router = createRouter({
 router.beforeEach((to, from, next) => {
     if (
         (to.path !== "/login" && to.path !== "/register") && // 排除登录和注册
-        !store.state.isLoggedIn // 判断是否已登录
+        !store.state.isLoggedIn // 使用 store 判断登录状态
     ) {
-        next("/login"); // 未登录重定向到登录页
+        next("/login"); // 如果未登录，重定向到登录页
     } else {
-        next(); // 继续访问目标页面
+        next(); // 继续其他页面
     }
 });
 
