cd0b5031c9b81df1419eb83a3ff6923344bea064
issac <issac@roliyal.com>
2025-03-24 14:43:09 +0800

    Update

--- Chapter4MicroserviceGovern/Unit2CodeLibrary/Microservice/front-guess/src/axiosInstance.js
+++ Chapter4MicroserviceGovern/Unit2CodeLibrary/Microservice/front-guess/src/axiosInstance.js
@@ -17,11 +17,13 @@ axiosInstance.interceptors.request.use(
 
         console.log('Adding headers:', { userId, authToken });  // 日志输出，检查请求头
 
-        // 在请求头中加入 X-User-ID 和 Authorization
+        // 在所有请求中都添加 X-User-ID 和 Authorization
+        // 只要 userId 存在就添加 X-User-ID
         if (userId) {
             config.headers['X-User-ID'] = userId;  // 添加 X-User-ID 请求头
         }
 
+        // 确保在所有请求中添加 Authorization 请求头
         if (authToken) {
             config.headers['Authorization'] = authToken;  // 添加 Authorization 请求头
         }
