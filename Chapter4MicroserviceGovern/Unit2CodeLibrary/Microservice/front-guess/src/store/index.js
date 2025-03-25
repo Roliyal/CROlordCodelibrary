0243361df4f92dfd29b3c2913967e390f02bbd1b
issac <issac@roliyal.com>
2025-03-25 17:57:39 +0800

    Update

--- Chapter4MicroserviceGovern/Unit2CodeLibrary/Microservice/front-guess/src/store/index.js
+++ Chapter4MicroserviceGovern/Unit2CodeLibrary/Microservice/front-guess/src/store/index.js
@@ -1,36 +1,5 @@
-// src/router/index.js
-import { createRouter, createWebHashHistory } from "vue-router";
-import LoginComponent from "../components/LoginComponent.vue";
-import GuessNumberComponent from "../components/GuessNumberComponent.vue";
-import ScoreboardComponent from "../components/ScoreboardComponent.vue";
-import RegisterComponent from "../components/RegisterComponent.vue";
-import store from "../store";  // 引入 Vuex store
-
-const routes = [
-    { path: "/login", component: LoginComponent },
-    { path: "/register", component: RegisterComponent },
-    { path: "/game", component: GuessNumberComponent },
-    { path: "/scoreboard", component: ScoreboardComponent },
-];
-
-const router = createRouter({
-    history: createWebHashHistory(process.env.BASE_URL),
-    routes,
-});
-
-router.beforeEach((to, from, next) => {
-    // 判断是否是登录或注册页面，或者是否已经登录
-    if (
-        (to.path !== "/login" && to.path !== "/register") &&  // 排除登录和注册页面
-        !store.state.isLoggedIn  // 未登录时
-    ) {
-        next("/login");  // 强制跳转到登录页
-    } else {
-        next();  // 继续访问目标页面
-    }
-});
-
-export default router;import { createStore } from 'vuex';
+// src/store/index.js
+import { createStore } from 'vuex';
 
 export default createStore({
     state: {
@@ -64,8 +33,8 @@ export default createStore({
         },
     },
     getters: {
-        isLoggedIn: (state) => state.isLoggedIn,
-        userId: (state) => state.userId,
-        authToken: (state) => state.authToken,
+        isLoggedIn: state => state.isLoggedIn,
+        userId: state => state.userId,
+        authToken: state => state.authToken,
     },
 });
