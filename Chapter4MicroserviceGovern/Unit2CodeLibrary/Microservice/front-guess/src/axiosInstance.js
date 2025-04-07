82613a4db6bb023b8d5fbbdf09f0008dac1a1357
issac <issac@roliyal.com>
2025-04-07 17:38:07 +0800

    Update

--- Chapter4MicroserviceGovern/Unit2CodeLibrary/Microservice/front-guess/src/axiosInstance.js
+++ Chapter4MicroserviceGovern/Unit2CodeLibrary/Microservice/front-guess/src/axiosInstance.js
@@ -3,30 +3,36 @@ import axios from 'axios';
 import store from './store';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://micro.roliyal.com',
+    baseURL: 'http://micro.roliyal.com', // 你的后端地址
     timeout: 10000,
-    withCredentials: true, // 允许携带 Cookie
+    withCredentials: true,              // 允许携带 Cookie
 });
 
 axiosInstance.interceptors.request.use(
     (config) => {
-        // 从 Vuex / localStorage 拿到 userId、authToken
-        let userId = store.state.userId || localStorage.getItem('userId');
-        let authToken = store.state.authToken || localStorage.getItem('authToken');
+        // 1. 优先从 Vuex 获取
+        let userId = store.state.userId;
+        let authToken = store.state.authToken;
 
-        if (userId) {
-            document.cookie = `x-pre-higress-tag=gray,X-User-ID=${userId}; path=/;`;
-        } else {
-            // 如果 userId 为空也可以写一个默认 Cookie
-            // document.cookie = 'x-pre-higress-tag=gray,X-User-ID=UNKNOWN; path=/;';
+        // 2. 如果 Vuex 没值，就从 localStorage 获取
+        if (!userId) {
+            userId = localStorage.getItem('userId');
+        }
+        if (!authToken) {
+            authToken = localStorage.getItem('authToken');
         }
 
-        // ② 如果后端要鉴权，保留 Authorization 头
+        // 3. 拿到就加到请求头里
+        if (userId) {
+            config.headers['X-User-ID'] = userId;
+            // 也写到Cookie（如果需要）
+            document.cookie = `X-User-ID=${userId}; path=/;`;
+        }
         if (authToken) {
             config.headers['Authorization'] = authToken;
         }
 
-        // ③ Content-Type
+        // 默认 Content-Type
         if (!config.headers['Content-Type']) {
             config.headers['Content-Type'] = 'application/json';
         }
