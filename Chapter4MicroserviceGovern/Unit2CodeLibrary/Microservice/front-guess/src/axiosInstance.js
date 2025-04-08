532a25a18beffb821b82a90d9deb69f1f65e2dd9
issac <issac@roliyal.com>
2025-04-08 15:20:15 +0800

    Update

--- Chapter4MicroserviceGovern/Unit2CodeLibrary/Microservice/front-guess/src/axiosInstance.js
+++ Chapter4MicroserviceGovern/Unit2CodeLibrary/Microservice/front-guess/src/axiosInstance.js
@@ -1,35 +1,39 @@
 import axios from 'axios';
-import store from './store';  // Vuex store
+import store from './store';
 
 const axiosInstance = axios.create({
     baseURL: 'http://micro.roliyal.com',
     timeout: 10000,
-    withCredentials: true, // 允许跨域带 Cookie
+    withCredentials: true, // 允许浏览器在跨域请求中发送 Cookie
 });
 
-axiosInstance.interceptors.request.use((config) => {
+axiosInstance.interceptors.request.use(config => {
     let userId = store.state.userId || localStorage.getItem('userId');
     let authToken = store.state.authToken || localStorage.getItem('authToken');
 
-    // 分成两个 Cookie
-    // 1) x-pre-higress-tag=gray
-    document.cookie = `x-pre-higress-tag=gray; path=/; SameSite=None; Secure`;
-
-    // 2) X-User-ID=xxx
+    // 1) X-User-ID Cookie
     if (userId) {
-        document.cookie = `X-User-ID=${userId}; path=/; SameSite=None; Secure`;
+        // 如果是同域HTTP
+        document.cookie = `X-User-ID=${userId}; path=/; SameSite=None`;
+        // 如果要跨域且HTTPS，需要 `; Secure`
+        //document.cookie = `X-User-ID=${userId}; path=/; SameSite=None; Secure`;
     }
 
-    // 如果后端要 Authorization
+    // 2) x-pre-higress-tag=gray
+    document.cookie = `x-pre-higress-tag=gray; path=/; SameSite=None`;
+    // 如果HTTPS: + Secure
+
+    // 3) Authorization header
     if (authToken) {
         config.headers['Authorization'] = authToken;
     }
 
-    // Content-Type
+    // 4) Content-Type
     if (!config.headers['Content-Type']) {
         config.headers['Content-Type'] = 'application/json';
     }
+
     return config;
-}, (error) => Promise.reject(error));
+}, error => Promise.reject(error));
 
 export default axiosInstance;
