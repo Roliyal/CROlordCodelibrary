fc16c180fe537e575ff2eeedf1953e2115b10da8
issac <issac@roliyal.com>
2025-04-08 15:11:17 +0800

    Update

--- Chapter4MicroserviceGovern/Unit2CodeLibrary/Microservice/front-guess/src/axiosInstance.js
+++ Chapter4MicroserviceGovern/Unit2CodeLibrary/Microservice/front-guess/src/axiosInstance.js
@@ -2,35 +2,34 @@ import axios from 'axios';
 import store from './store';  // Vuex store
 
 const axiosInstance = axios.create({
-    baseURL: 'http://micro.roliyal.com', // 你的后端地址
+    baseURL: 'http://micro.roliyal.com',
     timeout: 10000,
-    withCredentials: true,  // 允许跨域带 Cookie
+    withCredentials: true, // 允许跨域带 Cookie
 });
 
-axiosInstance.interceptors.request.use(
-    (config) => {
-        // 从 Vuex/localStorage 里拿 userId 和 token
-        let userId = store.state.userId || localStorage.getItem('userId');
-        let authToken = store.state.authToken || localStorage.getItem('authToken');
+axiosInstance.interceptors.request.use((config) => {
+    let userId = store.state.userId || localStorage.getItem('userId');
+    let authToken = store.state.authToken || localStorage.getItem('authToken');
 
-        // 设置 Cookie: x-pre-higress-tag=gray,X-User-ID=xxx
-        if (userId) {
-            document.cookie = `x-pre-higress-tag=gray,X-User-ID=${userId}; path=/; SameSite=None; Secure`;
-        }
+    // 分成两个 Cookie
+    // 1) x-pre-higress-tag=gray
+    document.cookie = `x-pre-higress-tag=gray; path=/; SameSite=None; Secure`;
 
-        // 如果后端要 Authorization
-        if (authToken) {
-            config.headers['Authorization'] = authToken;
-        }
+    // 2) X-User-ID=xxx
+    if (userId) {
+        document.cookie = `X-User-ID=${userId}; path=/; SameSite=None; Secure`;
+    }
 
-        // Content-Type
-        if (!config.headers['Content-Type']) {
-            config.headers['Content-Type'] = 'application/json';
-        }
+    // 如果后端要 Authorization
+    if (authToken) {
+        config.headers['Authorization'] = authToken;
+    }
 
-        return config;
-    },
-    (error) => Promise.reject(error)
-);
+    // Content-Type
+    if (!config.headers['Content-Type']) {
+        config.headers['Content-Type'] = 'application/json';
+    }
+    return config;
+}, (error) => Promise.reject(error));
 
 export default axiosInstance;
